f32c06819d0ebbb638e69d24c2f3f3f5eb345e9b
germo <germohn@gmail.com>
2017-12-13 14:17:14 +0200

    start of client

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      view: true
+    };
+    this.hanndleMyViewCLick = this.hanndleMyViewCLick.bind(this);
+    this.renderInitialView = this.renderInitialView.bind(this);
+    this.renderMyView = this.renderMyView.bind(this);
+  }
+
+  hanndleMyViewCLick(e) {
+
+    this.setState({
+      view: !this.state.view
+    })
+  }
+
+  renderInitialView() {
+    return (
+      <h2>test esinemne vaade</h2>
+    )
+  }
+
+
+  renderMyView() {
+    return (
+      <h2>test teine vaade</h2>
+    )
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+
+          <h1 className="App-title">TalentAtlas</h1>
+          <button onClick={this.hanndleMyViewCLick}> test</button>
+          {this.state.view  ? this.renderInitialView() : this.renderMyView()}
+
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
       </div>
     );
   }
